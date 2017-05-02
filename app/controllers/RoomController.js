b9dabff63d41030e799b8555cf57399da47b8363
Alexandre Surin <alexandre@hungryup.co>
2017-05-02 19:15:21 +0200

    WIP Authentication

--- app/controllers/RoomController.js
+++ app/controllers/RoomController.js
@@ -2,7 +2,7 @@ angular.module('myApp')
     .controller('RoomController',
       ($scope, socket, $http) => {
         $scope.rooms = [];
-        $http.get('http://localhost:3000/quotes')
+        $http.get('http://localhost:3000/api/quotes')
                 .then((response) => {
                   $scope.rooms = response.data;
                 });
@@ -12,7 +12,7 @@ angular.module('myApp')
             newRoom: room,
             username: $scope.username,
           });
-          $http.get(`http://localhost:3000/quotes/messages?room=${room.name}`)
+          $http.get(`http://localhost:3000/api/quotes/messages?room=${room.name}`)
                     .then((response) => {
                       $scope.messages = response.data;
                     });
--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,5 +1,14 @@
 angular.module('myApp')
     .controller('UserController',
-      ($scope) => {
-        $scope.user = 'Default User';
+      ($scope, $http) => {
+        $scope.credentials = {
+          email: '',
+          password: '',
+        };
+        $scope.login = (credentials) => {
+          $http.get(`http://localhost:3000/api/authenticate?email=${credentials.email}&password=${$scope.credentials.password}`)
+              .then((response) => {
+                // $scope.rooms = response.data;
+              });
+        };
       });
