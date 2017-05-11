caac406c7b532203295b4b3f65d264b245bfebe1
MissCaro <MissCaro>
2017-05-12 00:55:05 +0200

    Contact search & send request

--- app/controllers/ContactController.js
+++ app/controllers/ContactController.js
@@ -1,21 +1,32 @@
 angular.module('myApp')
     .controller('ContactController',
         ($scope, socket, $http) => {
+        $scope.information = {};
+            $scope.message = '';
             $scope.searchContact = (information) => {
-                $scope.userFound = false;
-                $scope.userNotFound = false;
+                $scope.message = '';
                 $http.get(`http://localhost:3000/api/contacts?contactEmail=${information.contactEmail}`)
                     .then((user) => {
-                        console.log(user);
-                        console.log(user.data);
                         if(user.data != null) {
                             $scope.userFound = user.data;
+                            $scope.userFound._id === $scope.user._id ? $scope.message = "It's you!" : '';
+                            $scope.user.friendRequestSent.find((contact) => {
+                                if ($scope.userFound._id === contact._id) {
+                                    $scope.message = "You already sent a request to this user";
+                                }
+                                });
+                            $scope.user.contacts.find((contact) => {
+                                if ($scope.userFound._id === contact._id) {
+                                    $scope.message = "User already in your contacts";
+                                }
+                            });
+                            if (!$scope.message) {
+                                $scope.notYouOrFriend = true;
+                            }
                         } else {
-                            console.log('bonjour');
-                            $scope.userNotFound = true;
+                            $scope.message = "We didn't find the user";
                         }
                     })
-
             };
             $scope.addContact = () => {
                 $http.post('http://localhost:3000/api/contacts', {
@@ -23,7 +34,9 @@ angular.module('myApp')
                     userId: $scope.user._id
                 })
                     .then((response) => {
-                        console.log(response);
+                        $scope.notYouOrFriend = false;
+                        $scope.message = "The request has been successfully sent";
+                        $scope.$parent.$parent.user = response.data;
                     })
-            }
+            };
         });
